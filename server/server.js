3c337b2b923fcdb80ad3a83d398f30128f2cb3f5
joerdixon <joseph35dixon@gmail.com>
2022-12-10 11:45:11 -0800

    server config

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./routes/htmlRoutes')(app);
+
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
